4c988dce7b19f41ae9296071f5d1b886ab748428
AyushDixit <ayushdixit891@gmail.com>
2021-01-17 22:36:21 +0530

    assignment 3 plot

--- Week 1/Assignment3/assignment3.py
+++ Week 1/Assignment3/assignment3.py
@@ -1,10 +1,14 @@
 import numpy as np
 import matplotlib.pyplot as pl
-x= np.arange(1,16,1)
+x= np.arange(1,17)
 print(x)
 y= [5,7,8,3,5,6,3,7,2,12,5,7,2,6,9,2]
 pl.xlabel("X axis")
 pl.ylabel("Y axis")
 pl.title("Epic info")
-pl.plot(x,y)
+pl.grid(color="white")
+ax = pl.axes()
+ax.set_facecolor("grey")
+pl.plot(x,y,'r')
 pl.show()
+
