c7bd0ed3fbe03475c7eed4eec9fc84e0065bf7de
AyushDixit <ayushdixit891@gmail.com>
2021-01-17 20:18:23 +0530

    solved task 2, solved task 3

---
+++ Week 1/task2.py
@@ -0,0 +1,8 @@
+def num_elements(l):
+    """
+    Returns number of elements in the list l
+    """
+    k= len(l)
+    return k
+"""Testing code for num_elements"""
+assert num_elements([2, 3, 4])==3
---
+++ Week 1/task3.py
@@ -0,0 +1,9 @@
+def reverse_list(l):
+    """
+    Returns the reverse of a list l
+    """
+    l.reverse()
+    return l
+        
+"""Testing code for reverse_list"""
+assert reverse_list([2, 3, 4])== [4, 3, 2]
