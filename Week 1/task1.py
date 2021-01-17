c48f21107b6883ebd765a9ff474beeb215791888
AyushDixit <ayushdixit891@gmail.com>
2021-01-17 17:41:21 +0530

    solved task 1

---
+++ Week 1/task1.py
@@ -0,0 +1,11 @@
+def last_element(l):
+    """
+    Returns last element of list l
+    """
+    k= l[-1]
+    return k
+
+"""Testing code for last_element"""
+assert last_element([1]) == 1
+assert last_element(["Hello", "World"]) == "World"
+
